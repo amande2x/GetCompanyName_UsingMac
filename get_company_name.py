e71d0d01af3ce45ecb315305b3b72b204752f0c5
amande2x <46036830+amande2x@users.noreply.github.com>
2019-08-12 11:47:55 -0700

    Update get_company_name.py

--- get_company_name.py
+++ get_company_name.py
@@ -11,12 +11,31 @@ import requests
 # api-endpoint 
 URL = "https://api.macaddress.io/v1?"
   
+def isMACaddressIsValid(input_str):
+    if input_str.count(":")!=5:
+        return False
+    for each_i in input_str.split(":"):
+        for char1 in each_i:
+            if char1.upper()>"F" or (char1.upper()<"A" and not char1.isdigit()) or len(each_i) !=2: 
+                return False
+    return True
+
+
 if len(sys.argv) < 2:
     print("Please provide MAC adress as input!!!")
     sys.exit(1)
 else:
     mac_address = sys.argv[1]
 
+while True:
+    try:
+        if isMACaddressIsValid(mac_address):
+            break
+        else: 
+            mac_address = input("Error:Please enter valid MAC address:")
+    except ValueError:
+        print("Invalid mac address")
+            
 print("input mac:",mac_address)
 
 # defining a params dict for the parameters to be sent to the API 
